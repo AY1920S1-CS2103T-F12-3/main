4f95e50be980fc4f747d8346626ab83488e6e71a
shiweing <shiweing1998@gmail.com>
2019-10-16 23:27:57 +0800

    Implement note filtering by string keyword

--- src/main/java/tagline/logic/commands/note/ListNoteCommand.java
+++ src/main/java/tagline/logic/commands/note/ListNoteCommand.java
@@ -3,8 +3,13 @@ package tagline.logic.commands.note;
 import static java.util.Objects.requireNonNull;
 import static tagline.model.note.NoteModel.PREDICATE_SHOW_ALL_NOTES;
 
+import java.util.Arrays;
+import java.util.List;
+
 import tagline.logic.commands.CommandResult;
+import tagline.logic.commands.exceptions.CommandException;
 import tagline.model.Model;
+import tagline.model.note.NoteContainsKeywordsPredicate;
 
 /**
  * Lists all contacts in the address book to the user.
@@ -14,6 +19,8 @@ public class ListNoteCommand extends NoteCommand {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all notes";
+    public static final String MESSAGE_KEYWORD_SUCCESS = "Listed notes for keyword: %1$s";
+    public static final String MESSAGE_KEYWORD_EMPTYLIST = "No notes matching keyword: %1$s";
 
     private final Filter filter;
 
@@ -25,16 +32,33 @@ public class ListNoteCommand extends NoteCommand {
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
         if (filter == null) { // No filter, list all notes
             model.updateFilteredNoteList(PREDICATE_SHOW_ALL_NOTES);
             return new CommandResult(MESSAGE_SUCCESS);
-        } else { // list if note contains keyword
-
-            return new CommandResult("Listed notes for keyword: " + filter.getFilterValue());
+        } else { // list if note contains keyword (filter.filterType.equals(KEYWORD))
+            return filterAndListByKeyword(model);
         }
+
+        /* TODO Implement filter by tag */
+    }
+
+    /**
+     * Filter note list by String keyword
+     */
+    private CommandResult filterAndListByKeyword(Model model) throws CommandException {
+        List<String> keywordList = Arrays.asList(filter.filterValue.split(" "));
+        NoteContainsKeywordsPredicate predicate = new NoteContainsKeywordsPredicate(keywordList);
+
+        model.updateFilteredNoteList(predicate);
+
+        if (model.getFilteredNoteList().size() == 0) {
+            throw new CommandException(String.format(MESSAGE_KEYWORD_EMPTYLIST, filter.filterValue));
+        }
+
+        return new CommandResult(String.format(MESSAGE_KEYWORD_SUCCESS, filter.filterValue));
     }
 
     /**
