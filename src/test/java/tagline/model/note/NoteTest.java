e4e3687792a29c9cf03df66f8565ae421dcdb1f5
shiweing <shiweing1998@gmail.com>
2019-10-16 19:35:15 +0800

    Fix test errors

--- src/test/java/tagline/model/note/NoteTest.java
+++ src/test/java/tagline/model/note/NoteTest.java
@@ -12,8 +12,10 @@ import static tagline.logic.commands.NoteCommandTestUtil.VALID_CONTENT_INCIDENT;
 //import static tagline.logic.commands.NoteCommandTestUtil.VALID_CONTENT_PROTECTOR;
 import static tagline.logic.commands.NoteCommandTestUtil.VALID_NOTEID_INCIDENT;
 //import static tagline.logic.commands.NoteCommandTestUtil.VALID_NOTEID_PROTECTOR;
+import static tagline.logic.commands.NoteCommandTestUtil.VALID_TAG_AVENGERS;
 import static tagline.logic.commands.NoteCommandTestUtil.VALID_TIMECREATED_INCIDENT;
 //import static tagline.logic.commands.NoteCommandTestUtil.VALID_TIMECREATED_PROTECTOR;
+import static tagline.logic.commands.NoteCommandTestUtil.VALID_TIMECREATED_PROTECTOR;
 import static tagline.logic.commands.NoteCommandTestUtil.VALID_TIMELASTUPDATED_INCIDENT;
 import static tagline.logic.commands.NoteCommandTestUtil.VALID_TITLE_INCIDENT;
 import static tagline.testutil.Assert.assertThrows;
@@ -42,46 +44,62 @@ public class NoteTest {
         // null -> returns false
         assertFalse(PROTECTOR.isSameNote(null));
 
-        //// different phone and email -> returns false
-        //Note editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_INCIDENT)
-        //    .withTimeCreated(VALID_TIMECREATED_INCIDENT).build();
-        //assertFalse(PROTECTOR.isSameNote(editedProtector));
+        // different id -> returns false
+        Note editedProtector = new NoteBuilder(PROTECTOR).withNoteId(VALID_NOTEID_INCIDENT).build();
+        assertFalse(PROTECTOR.isSameNote(editedProtector));
+
+        // different title -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTitle(VALID_TITLE_INCIDENT).build();
+        assertTrue(PROTECTOR.isSameNote(editedProtector));
+
+        // different content -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_INCIDENT).build();
+        assertTrue(PROTECTOR.isSameNote(editedProtector));
+
+        // different tag -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTags(VALID_TAG_AVENGERS).build();
+        assertTrue(PROTECTOR.isSameNote(editedProtector));
+
+        // different timecreated -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTimeCreated(VALID_TIMECREATED_INCIDENT).build();
+        assertTrue(PROTECTOR.isSameNote(editedProtector));
+
+        // different timeLastEdited -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTimeLastUpdated(VALID_TIMELASTUPDATED_INCIDENT).build();
+        assertTrue(PROTECTOR.isSameNote(editedProtector));
+    }
+
+    @Test
+    public void isUniqueNote() {
+        // same object -> returns true
+        assertTrue(PROTECTOR.isUniqueNote(PROTECTOR));
+
+        // null -> returns false
+        assertFalse(PROTECTOR.isUniqueNote(null));
 
         // different id -> returns false
         Note editedProtector = new NoteBuilder(PROTECTOR).withNoteId(VALID_NOTEID_INCIDENT).build();
-        //.withTimeCreated(VALID_TIMECREATED_PROTECTOR).build();
-        assertFalse(PROTECTOR.isSameNote(editedProtector));
+        assertFalse(PROTECTOR.isUniqueNote(editedProtector));
 
-        //// different content -> returns false
-        //editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_INCIDENT)
-        //    .withTimeCreated(VALID_TIMECREATED_PROTECTOR).build();
-        //assertFalse(PROTECTOR.isSameNote(editedProtector));
+        // different title -> returns false
+        editedProtector = new NoteBuilder(PROTECTOR).withTitle(VALID_TITLE_INCIDENT).build();
+        assertFalse(PROTECTOR.isUniqueNote(editedProtector));
 
-        //// different timecreated -> returns false
-        //editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_PROTECTOR)
-        //        .withTimeCreated(VALID_TIMECREATED_INCIDENT).build();
-        //assertFalse(PROTECTOR.isSameNote(editedProtector));
+        // different content -> returns false
+        editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_INCIDENT).build();
+        assertFalse(PROTECTOR.isUniqueNote(editedProtector));
 
-        //// different content and timecreated -> returns false
-        //editedProtector = new NoteBuilder(PROTECTOR).withContent(VALID_CONTENT_INCIDENT)
-        //        .withTimeCreated(VALID_TIMECREATED_INCIDENT).build();
-        //assertFalse(PROTECTOR.isSameNote(editedProtector));
+        // different tag -> returns false
+        editedProtector = new NoteBuilder(PROTECTOR).withTags(VALID_TAG_AVENGERS).build();
+        assertFalse(PROTECTOR.isUniqueNote(editedProtector));
 
-        //// same name, same phone, different attributes -> returns true
-        editedProtector = new NoteBuilder(PROTECTOR).withTimeLastUpdated(VALID_TIMELASTUPDATED_INCIDENT)
-                //.withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND).build();
-        assertTrue(PROTECTOR.isSameNote(editedProtector));
+        // different timecreated -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTimeCreated(VALID_TIMECREATED_INCIDENT).build();
+        assertTrue(PROTECTOR.isUniqueNote(editedProtector));
 
-        //// same name, same email, different attributes -> returns true
-        //editedProtector = new NoteBuilder(PROTECTOR).withPhone(VALID_PHONE_BOB).withAddress(VALID_ADDRESS_BOB)
-        //        .withTags(VALID_TAG_HUSBAND).build();
-        //assertTrue(PROTECTOR.isSameNote(editedProtector));
-
-        // same name, same phone, same email, different attributes -> returns true
-        //editedProtector = new NoteBuilder(PROTECTOR).withAddress(VALID_ADDRESS_BOB)
-        //           .withTags(VALID_TAG_HUSBAND).build();
-        //assertTrue(PROTECTOR.isSameNote(editedProtector));
+        // different timeLastEdited -> returns true
+        editedProtector = new NoteBuilder(PROTECTOR).withTimeLastUpdated(VALID_TIMELASTUPDATED_INCIDENT).build();
+        assertTrue(PROTECTOR.isUniqueNote(editedProtector));
     }
 
     @Test
