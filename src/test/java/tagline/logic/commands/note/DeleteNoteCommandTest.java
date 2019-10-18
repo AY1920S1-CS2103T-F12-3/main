95ee3e599fef31039c7b853967d783561d17fa6b
shiweing <shiweing1998@gmail.com>
2019-10-19 01:12:57 +0800

    Implement invalid note id test for DeleteNoteCommand

--- src/test/java/tagline/logic/commands/note/DeleteNoteCommandTest.java
+++ src/test/java/tagline/logic/commands/note/DeleteNoteCommandTest.java
@@ -3,16 +3,21 @@ package tagline.logic.commands.note;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tagline.logic.commands.CommandResult.*;
 import static tagline.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tagline.logic.commands.NoteCommandTestUtil.NON_EXISTING_NOTE_ID;
 import static tagline.testutil.Assert.assertThrows;
 import static tagline.testutil.TypicalIndexes.INDEX_FIRST;
 import static tagline.testutil.TypicalNotes.getTypicalNoteBook;
 
 import org.junit.jupiter.api.Test;
+import tagline.commons.core.Messages;
 import tagline.logic.commands.CommandResult;
+import tagline.logic.commands.NoteCommandTestUtil;
+import tagline.logic.parser.note.NoteParserUtil;
 import tagline.model.Model;
 import tagline.model.ModelManager;
 import tagline.model.UserPrefs;
 import tagline.model.note.Note;
+import tagline.model.note.NoteId;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -57,6 +62,13 @@ class DeleteNoteCommandTest {
        assertCommandSuccess(deleteNoteCommand, model, expectedMessage, DELETE_NOTE_COMMAND_VIEW_TYPE, expectedModel);
    }
 
+   @Test
+   public void execute_nonExistingNoteId_throwsCommandException() {
+        NoteId nonExistingNoteId = NON_EXISTING_NOTE_ID;
+        DeleteNoteCommand deleteNoteCommand = new DeleteNoteCommand(nonExistingNoteId);
+
+       NoteCommandTestUtil.assertCommandFailure(deleteNoteCommand, model, Messages.MESSAGE_INVALID_NOTE_INDEX);
+   }
     /**
      * Updates {@code model}'s filtered list to show no one.
      */
