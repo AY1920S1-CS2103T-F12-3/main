a798e9a032b5de32ccc40be1d7e66f6c7c990000
shiweing <shiweing1998@gmail.com>
2019-10-26 13:41:58 +0800

    Add DeleteNoteParserTest

---
+++ src/test/java/tagline/logic/parser/note/DeleteNoteParserTest.java
@@ -0,0 +1,31 @@
+package tagline.logic.parser.note;
+
+import org.junit.jupiter.api.Test;
+import tagline.logic.commands.note.DeleteNoteCommand;
+import tagline.model.note.NoteId;
+
+import static tagline.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static tagline.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static tagline.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+class DeleteNoteParserTest {
+
+    private DeleteNoteParser parser = new DeleteNoteParser();
+
+    @Test
+    public  void parse_validArgs_returnsDeleteNoteCommand() {
+        assertParseSuccess(parser, "1", new DeleteNoteCommand(new NoteId(1)));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteNoteCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-1", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteNoteCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "10a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteNoteCommand.MESSAGE_USAGE));
+    }
+}
